9c11b25ea6f05673359b6a1b64c5ec6e9580b9dc
matrunych <47101257+matrunych@users.noreply.github.com>
2019-04-20 22:35:16 +0300

    adt

---
+++ adt_dict.py
@@ -0,0 +1,28 @@
+class AssociativeArray:
+    def __init__(self):
+        self.array = []
+
+    def insert(self, key, value):
+        for pair in self.array:
+            if pair[0] == key:
+                return 'Duplicate key'
+        return self.array.append((key, value))
+
+    def remove(self, key):
+        for pair in self.array:
+            if pair[0] == key:
+                return self.array.remove(pair)
+        return 'Pair does not exist'
+
+    def update(self, key, value):
+        for pair in self.array:
+            if pair[0] == key:
+                self.array.remove(pair)
+                return self.array.append((key, value))
+        return 'Key does not exist'
+
+    def lookup(self, key):
+        for pair in self.array:
+            if pair[0] == key:
+                return pair[1]
+        return 'Key does not exist'
